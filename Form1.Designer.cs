72c5a33d08ba099000bdbb1733d26a2061238ad7
WIN_NT\x16j059 <x16j059@chiba-fjb.ac.jp>
2017-05-19 11:40:01 +0900

    中

---
+++ Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace Example11
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbxYear = new System.Windows.Forms.TextBox();
+            this.btnJudge = new System.Windows.Forms.Button();
+            this.LbLResult = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 69);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "西暦";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(178, 69);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(17, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "年";
+            // 
+            // tbxYear
+            // 
+            this.tbxYear.Location = new System.Drawing.Point(60, 66);
+            this.tbxYear.Name = "tbxYear";
+            this.tbxYear.Size = new System.Drawing.Size(100, 19);
+            this.tbxYear.TabIndex = 2;
+            this.tbxYear.TextChanged += new System.EventHandler(this.tbxYear_TextChanged);
+            // 
+            // btnJudge
+            // 
+            this.btnJudge.Location = new System.Drawing.Point(201, 64);
+            this.btnJudge.Name = "btnJudge";
+            this.btnJudge.Size = new System.Drawing.Size(75, 23);
+            this.btnJudge.TabIndex = 3;
+            this.btnJudge.Text = "判定";
+            this.btnJudge.UseVisualStyleBackColor = true;
+            // 
+            // LbLResult
+            // 
+            this.LbLResult.AutoSize = true;
+            this.LbLResult.Location = new System.Drawing.Point(86, 123);
+            this.LbLResult.Name = "LbLResult";
+            this.LbLResult.Size = new System.Drawing.Size(37, 12);
+            this.LbLResult.TabIndex = 4;
+            this.LbLResult.Text = "(空白)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.LbLResult);
+            this.Controls.Add(this.btnJudge);
+            this.Controls.Add(this.tbxYear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "うるう年判定";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbxYear;
+        private System.Windows.Forms.Button btnJudge;
+        private System.Windows.Forms.Label LbLResult;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example11
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tbxYear_TextChanged(object sender, EventArgs e)
+        {
+            int year = int.Parse(tbxYear.Text);
+            if (year % 4 != 0)
+                LbLResult.Text = "①平成です";
+            else
+            {
+                if (year % 100 == 0)
+                {
+                    if (year % 400 == 0)
+                        LbLResult.Text = "②うるう年です";
+                    else
+                        LbLResult.Text = "③平年です";
+                }
+                else
+                    LbLResult.Text = "④うるう年です";
+            }
+        }
+    }
+}
